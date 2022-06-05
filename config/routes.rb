277d360c4f94ae2374b9b9ba7f9768a7a9a52cf2
denvermullets <devmullets@gmail.com>
2022-06-05 10:55:34 -0400

    chore(routes): remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :leagues, only: [:create]
-      # post '/league', to: 'leagues#create', as: :league_create
-
     end
   end
 end
