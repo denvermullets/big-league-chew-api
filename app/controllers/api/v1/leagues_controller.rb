616d2c269c0a18bd43a26a8a50731cfb2ccbd1ea
denvermullets <devmullets@gmail.com>
2022-06-05 10:55:17 -0400

    feat(leagues): add create route

--- app/controllers/api/v1/leagues_controller.rb
+++ app/controllers/api/v1/leagues_controller.rb
@@ -1,7 +1,18 @@
 module Api
   module V1
+    # currently only accepting lat/long co-ord's, could open up to street address
     class LeaguesController < ApplicationController
       # do stuffs
+      def create
+        league = League.create(create_league_params)
+        render json: league
+      end
+
+      private
+
+      def create_league_params
+        params.require(:league).permit(:league_name, :price, :latitude, :longitude)
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :leagues
+      resources :leagues, only: [:create]
+      # post '/league', to: 'leagues#create', as: :league_create
+
     end
   end
 end
